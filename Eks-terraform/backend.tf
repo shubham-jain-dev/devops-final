909f9a96ad7118f1e938f1f0528dffe86240955b
Shubham Jain <161154034+shubham-jain-dev@users.noreply.github.com>
2025-05-08 17:44:08 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devops-project" # Replace with your actual S3 bucket name
+    bucket = "devops-project-final" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
